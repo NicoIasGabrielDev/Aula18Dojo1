b759187b591b3eab376f171fc5522c9e6ff2cd35
nicolasgabrieloficial <61585068+nicolasgabrieloficial@users.noreply.github.com>
2020-07-01 03:41:18 -0300

    Add files via upload

---
+++ Aula18Dojo1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula18Dojo1
+{
+    // 
+    //
+    //Pq está dando erro aqui?
+    //
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Jogador neymar = new Jogador();
+            neymar.Nome = "Neymar Jr.";
+            neymar.Posicao = "Atacante";
+            neymar.Nascimento = DateTime.Parse("05/02/1992");            
+            //ação de mostrar o dados e calcular idade
+            neymar.MostrarDados();
+            Console.WriteLine( neymar.CalcularIdade() );
+            Console.WriteLine( neymar.CalcularAposentadoria() );
+
+        }
+    }
+}
